259d220c9c748e76adcfe5766194a950adc39ba2
大二 <daer.zsk@antfin.com>
2018-12-09 01:02:22 +0800

    OK, it can run

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Homepage from './pages/homepage';
+
+ReactDOM.render(<Homepage></Homepage>, document.querySelector('#app'));
---
+++ src/pages/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Homepage extends React.Component {
+
+  render() {
+    return (
+      <div>Hello Webpack basic</div>
+    )
+  }
+}
